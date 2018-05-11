383d77683f739fdeca5ed0e5a0fbb420cab1f10c
joaerazogo <joaerazogo@unal.edu.co>
2018-05-11 11:30:13 -0500

    parcial 2

--- app/routes/authors/author.js
+++ app/routes/authors/author.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model({  id }){
+    console.log(id);
+    return this.store.query('author', {
+      orderBy: 'id',
+      equalTo: id,
+      limitToFirst: 1,
+    }).then((author) => {
+      return author.get('firstObject');
+    });
+  }
 });
